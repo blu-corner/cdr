d3bb7b5f62cd7753c408f2b874f47bbd4339e5fe
Alejandro Navarro <alejandro.navarro@neueda.com>
2018-11-07 10:14:45 +0000

    Adding csharp examples to correct folder

---
+++ examples/csharp/run.sh
@@ -0,0 +1,8 @@
+#
+# Copyright 2014-2018 Neueda Ltd.
+#
+#!/usr/bin/env bash
+set -e
+
+cp `pwd`/../../../lib/csharp/Cdr.dll .
+LD_LIBRARY_PATH=`pwd`/../../../lib:`pwd`/../../../lib/csharp:$LD_LIBRARY_PATH ./test.exe
