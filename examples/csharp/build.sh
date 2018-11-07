7076bef1ac860e9caeea2093a6e11c9218c56034
Alejandro Navarro <alejandro.navarro@neueda.com>
2018-11-07 14:42:30 +0000

    Homogenizing dll filename for all platforms and adding csc compiler for windows

--- examples/csharp/build.sh
+++ examples/csharp/build.sh
@@ -4,5 +4,5 @@
 #!/usr/bin/env bash
 set -e
 
-cp `pwd`/../../../lib/csharp/Cdr.dll .
-LD_LIBRARY_PATH=`pwd`/../../../lib:`pwd`/../../../lib/csharp:$LD_LIBRARY_PATH mcs -out:test.exe -reference:Cdr.dll example.cs
+cp `pwd`/../../../lib/csharp/CdrBindings.dll .
+LD_LIBRARY_PATH=`pwd`/../../../lib:`pwd`/../../../lib/csharp:$LD_LIBRARY_PATH mcs -out:test.exe -reference:CdrBindings.dll example.cs
